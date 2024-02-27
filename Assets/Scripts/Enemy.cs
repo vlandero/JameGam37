98b00bb5c0d93e51d51c1fcaec73948ac1757947
Vlad Rosu <86161545+vlandero@users.noreply.github.com>
2024-02-28 00:18:00 +0200

    Create enemy (#6)
    * started creating enemy
    
    * fix errors in master

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Obstacle
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Deactivate()
     {
-        
+        base.Deactivate();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Activate()
     {
-        
+        base.Activate();
     }
 }
