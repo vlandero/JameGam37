e97dee19125d189022956243cffc92bc3dc5199f
Simion Petrica <95485582+Petrica81@users.noreply.github.com>
2024-02-27 12:03:34 +0200

    movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float jumpForce = 300f;
+
+    [SerializeField]
+    private Transform feetPosition;
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    private float horizontalDirection;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {      
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+        }
+    }
+    void FixedUpdate()
+    {
+        if (!IsOnWall() || IsGrounded()) {
+            rb.velocity = new Vector2(horizontalDirection * speed, rb.velocity.y);
+        }
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapBox(feetPosition.position, new Vector2(0.1f, 0.1f), 0f, groundLayer);
+    }
+    private bool IsOnWall()
+    {
+        return Physics2D.OverlapBox(transform.position, new Vector2(1.2f, 1f), 0f, groundLayer);
+    }
+}
