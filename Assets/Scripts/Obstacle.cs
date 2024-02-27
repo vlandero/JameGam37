4b3ce25fb7b3c4927450c48399a145147c21f5fc
Vlad Rosu <86161545+vlandero@users.noreply.github.com>
2024-02-27 23:36:20 +0200

    Add obstacles (#3)
    * add player attached to the ground and demo level in sample scene
    
    * add obstacles
    
    * add platform
    
    * fix bug with main platfor
    
    * added switch realities
    
    * fix layer bugs

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Obstacle : MonoBehaviour
+{
+    [SerializeField] protected bool isActive;
+
+    protected MainCamera mainCamera;
+    protected bool isOnScreen;
+
+    protected BoxCollider2D boxCollider;
+    protected SpriteRenderer spriteRenderer;
+
+    protected void Start()
+    {
+        mainCamera = Camera.main.GetComponent<MainCamera>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        isOnScreen = false;
+        Deactivate();
+        // we don't need to activate here because at the first update frame it will enter the screen area and it will activate if necessary
+    }
+
+    protected void Update()
+    {
+        if(!isOnScreen && transform.position.x < mainCamera.transform.position.x + mainCamera.width / 2)
+        {
+            EnteredScreenArea();
+        }
+    }
+
+    public bool IsActive()
+    {
+        return isActive;
+    }
+
+    public void SwitchReality()
+    {
+        SetActive(!isActive);
+    }
+
+    public void SetActive(bool active)
+    {
+        isActive = active;
+        if(isActive && isOnScreen)
+        {
+            Activate();
+        }
+        else
+        {
+            Deactivate();
+        }
+    }
+
+    protected void EnteredScreenArea()
+    {
+        isOnScreen = true;
+        if(isActive)
+        {
+            Activate();
+        }
+    }
+
+    virtual protected void Deactivate()
+    {
+        var color = spriteRenderer.color;
+        spriteRenderer.color = new Color(color.r, color.g, color.b, .5f);
+        boxCollider.enabled = false;
+    }
+
+    virtual protected void Activate()
+    {
+        var color = spriteRenderer.color;
+        spriteRenderer.color = new Color(color.r, color.g, color.b, 1);
+        boxCollider.enabled = true;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,6 @@ public class PlayerController : MonoBehaviour
     private float speed = 5f;
     [SerializeField]
     private float jumpForce = 300f;
-
     [SerializeField]
     private Transform feetPosition;
     [SerializeField]
@@ -18,46 +17,105 @@ public class PlayerController : MonoBehaviour
     private Transform rightPosition;
     [SerializeField]
     private LayerMask groundLayer;
+    [SerializeField]
+    private LayerMask obstacleLayer;
+    [SerializeField]
+    private ObstacleManager obstacleManager;
 
     private float horizontalDirection;
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+    private Transform currentGround;
     void Start()
     {      
         rb = GetComponent<Rigidbody2D>();
+        currentGround = null;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        horizontalDirection = Input.GetAxisRaw("Horizontal");
+        if(Input.GetButtonDown("Switch Reality"))
+        {
+            Debug.Log("Switch Reality");
+            obstacleManager.SwitchReality();
+        }
+        if(Input.GetButtonDown("Stop Tire"))
+        {
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        }
+        horizontalDirection = Input.GetAxisRaw("Horizontal");
+        bool isGrounded = IsGrounded();
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+        }
+        else
+        {
+            AttachToFloor(isGrounded);
         }
     }
     void FixedUpdate()
     {
-        if (horizontalDirection >= 0 && !IsWallRight())
+        bool isWallLeft = IsWallLeft();
+        bool isWallRight = IsWallRight();
+        if (horizontalDirection >= 0 && !isWallRight)
         {
             rb.velocity = new Vector2(horizontalDirection * speed, rb.velocity.y);
         }
-        if (horizontalDirection <= 0 && !IsWallLeft())
+        if (horizontalDirection <= 0 && !isWallLeft)
         {
             rb.velocity = new Vector2(horizontalDirection * speed, rb.velocity.y);
+        }
+    }
+    private void AttachToFloor(bool isGrounded)
+    {
+        if (isGrounded)
+        {
+            if (currentGround != null)
+            {
+                transform.parent = currentGround;
+            }
+        }
+        else
+        {
+            transform.parent = null;
         }
     }
     private bool IsGrounded()
     {
-        return Physics2D.OverlapBox(feetPosition.position, new Vector2(0.98f, 0.1f), 0f, groundLayer);
+        Collider2D groundCol = Physics2D.OverlapBox(feetPosition.position, new Vector2(0.98f, 0.1f), 0f, groundLayer);
+        Collider2D obstacleCol = Physics2D.OverlapBox(feetPosition.position, new Vector2(0.98f, 0.1f), 0f, obstacleLayer);
+        if (groundCol != null)
+        {
+            currentGround = groundCol.transform;
+            return true;
+        }
+        else if (obstacleCol != null)
+        {
+            currentGround = obstacleCol.transform;
+            return true;
+        }
+        currentGround = null;
+        return false;
     }
     private bool IsWallLeft()
     {
-        return Physics2D.OverlapBox(leftPosition.position, new Vector2(0.1f, 0.98f), 0f, groundLayer);
+        return Physics2D.OverlapBox(leftPosition.position, new Vector2(0.1f, 0.98f), 0f, obstacleLayer);
     }
     private bool IsWallRight()
     {
-        return Physics2D.OverlapBox(rightPosition.position, new Vector2(0.1f, 0.98f), 0f, groundLayer);
+        return Physics2D.OverlapBox(rightPosition.position, new Vector2(0.1f, 0.98f), 0f, obstacleLayer);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("loss"))
+        {
+            Debug.Log("Game Over");
+        }
+        else if (collision.CompareTag("end"))
+        {
+            Debug.Log("You Win");
+        }
     }
 }
